b67b11a949b1cbae24de815bb048ff65cbc78925
dadayama <dadayamism@gmail.com>
2021-07-28 22:02:10 +0900

    ページをscreensに分ける

--- App.tsx
+++ App.tsx
@@ -1,30 +1,4 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { AppBar } from './src/components/AppBar';
-import { MemoList } from './src/components/MemoList';
-import { CircleButton } from './src/components/CircleButton';
-import { FloatingAction } from './src/components/FloatingAction';
+import { MemoListScreen } from './src/screens/MemoListScreen';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f0f4f8',
-  },
-});
-
-const memoListItems = [
-  { title: '買い物リスト1', date: new Date() },
-  { title: '買い物リスト2', date: new Date() },
-];
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <AppBar />
-      <MemoList items={memoListItems} />
-      <FloatingAction>
-        <CircleButton>+</CircleButton>
-      </FloatingAction>
-    </View>
-  );
-}
+export default () => <MemoListScreen />;
---
+++ src/screens/MemoListScreen.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { AppBar } from '../components/AppBar';
+import { MemoList } from '../components/MemoList';
+import { CircleButton } from '../components/CircleButton';
+import { FloatingAction } from '../components/FloatingAction';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f4f8',
+  },
+});
+
+const memoListItems = [
+  { title: '買い物リスト1', date: new Date() },
+  { title: '買い物リスト2', date: new Date() },
+];
+
+export const MemoListScreen: React.FC = () => (
+  <View style={styles.container}>
+    <AppBar />
+    <MemoList items={memoListItems} />
+    <FloatingAction>
+      <CircleButton>+</CircleButton>
+    </FloatingAction>
+  </View>
+);
